1918b3d0219dda23e160bd8fcb32481547e1a4d6
Michael Hull <mikehulluk@gmail.com>
2015-09-26 22:38:10 +0100

    Debug Time!

--- src/processmanager/central_processmgr_tracker.py
+++ src/processmanager/central_processmgr_tracker.py
@@ -74,322 +74,45 @@ connected_processes = {}
 
 
 
-#  #@asyncio.coroutine
-#  def generate_content_cr():
-#      print ("Starting generate content:")
-#      sys.stdout.flush()
-#  
-#      while(1):
-#          time.sleep(1)
-#  
-#          with websockets_lock:
-#              print ("Sending content..")
-#              print ("NWebsockets: %d" % len(websockets))
-#              for websocket in websockets:
-#                  pass
-#  
-#                  #if websocket.open:
-#                  #    yield from websocket.send(send_data)
-#  
-#  
-#  def generate_content_thread():
-#      loop = asyncio.new_event_loop()
-#      asyncio.set_event_loop(loop)
-#      asyncio.get_event_loop().run_until_complete(generate_content_cr())
-#      asyncio.get_event_loop().run_forever()
-#  
-#  
-#  
-#  
-#  print("Starting autogenerate thread")
-#  tr_autogen = Thread(target=generate_content_cr)
-#  # (Starting as a daemon means the thread will not prevent the process from exiting.)
-#  #tr_autogen.daemon = True
-#  tr_autogen.start()
-#  
-#  print("Looping")
-#  while(1):
-#      time.sleep(1)
-#  
-#  
-#  print("jlkjl")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class ThreadedTCPRequestHandler(socketserver.StreamRequestHandler):
-
-    def handle(self):
-
-        self.procmgr_id = None
-
-        while True:
-            # Msgs are "SIZE:PORT:CONTENTS"
-            print ("Waiting for msg...")
-
-            msg_size = self.rfile.readline()
-            if not msg_size:
-                return
-            msg_size = int(msg_size)
-            msg_subport = self.rfile.readline()
-            if not msg_subport:
-                return
-
-            buff = b''
-            while len(buff) < msg_size:
-                to_read = msg_size - len(buff)
-                x = self.rfile.read(to_read)
-                if not x:
-                    print( 'Connection closed')
-                    return
-                buff += x
-
-            subport = int(msg_subport.decode('utf-8').strip() )
-            msg = buff.decode('utf-8')
-
-
-            print("Sending empty message to all websockets:")
-            for websocket in websockets:
-                print("  -- %s" % websocket)
-                websocket.send("[]")
-
-
-            #print ('Msg read OK: %d bytes over subport: %d' % ( len(msg), subport ) )
-            #print(self.handleMsgMJHX)
-            self.handleMsgMJHX( subport=subport, msg=msg )
-            #print ("Handled OK!\n")
-
-
-
-    def handleMsgMJHX(self, subport, msg):
-        print ('Handling Message Port:%d Length:%d' %(subport, len(msg)) )
-        print (msg)
-        print ('\n')
-
-        if subport == 0:
-            print("Hooking in!")
-            msg = json.loads(msg)
-            print (msg)
-
-            with websockets_lock:
-
-                print("New Process Mgr Connected")
-
-                if connected_processes:
-                    self.procmgr_id =  max( [ mgr['id'] for mgr in connected_processes.values() ]) + 1
-                    next_proc_id = max( [ proc['id']  for mgr in connected_processes.values() for proc in mgr['processes'] ]) + 1
-                else:
-                    self.procmgr_id = 0
-                    next_proc_id = 0
-
-                msg['id'] = self.procmgr_id
-                for (i,process) in enumerate(msg['processes']):
-                    process['id'] = next_proc_id + i
-
-                # Add to the list:
-                connected_processes[self.procmgr_id] = msg
-                pp.pprint(connected_processes)
-                print("Connected to %d" % len(connected_processes) )
-
-
-        else:
-            print("Forwarding message....")
-            subport_str = str(subport)
-            # Lookup the port:
-            with websockets_lock:
-                process, pipe_name = None,None
-                for proc in connected_processes[self.procmgr_id]['processes']:
-                    #print ("AA")
-                    #print (proc['outpipes'])
-                    if subport_str in proc['outpipes']:
-                        pipe_name = proc['outpipes'][subport_str]
-                        process=proc
-                        break
-                    #print ("BB")
-                #print("Subport_str: %s" %subport_str)
-                #print("Pipename: %s" %pipe_name)
-
-                assert(pipe_name)
-                assert(proc)
-                #print ("Found pipename: %s", pipe_name)
-
-
-                # Ok, we can now send out a packet to each websocket:
-                std_pkt = [ {'msg-type':'output','process_id':process['id'], 'pipe':pipe_name, 'contents':msg}]
-                std_pkt_json = json.dumps(std_pkt)
-
-
-                #with websockets_lock:
-                #print ("Sending content..")
-                #print ("NWebsockets: %d" % len(websockets))
-                for (websocket, ws_procmgr_id) in websockets.items():
-                    #print([self.procmgr_id, ws_procmgr_id])
-                    if self.procmgr_id != ws_procmgr_id:
-                        continue
-
-                    if websocket.open:
-                        print("Forwarding to websocket....%d", (websocket))
-                        #assert(0)
-                        websocket.send(std_pkt_json)
-                        print("Sent...")
-
-    def finish(self):
-        print ("Closing up socket")
-        if not self.procmgr_id:
-            return
-        with websockets_lock:
-            del connected_processes[self.procmgr_id]
-
-
-
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
-
-
-
-def build_tcpserver():
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", common.Ports.ProcessMgrs
-
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    ip, port = server.server_address
-
-    # Start a thread with the server -- that thread will then start one
-    # more thread for each request
-    server_thread = threading.Thread(target=server.serve_forever)
-    # Exit the server thread when the main thread terminates
-    server_thread.daemon = True
-    server_thread.start()
-
-
-
-print ("Launching tcp server:")
-tr_tcp = Thread(target=build_tcpserver)
-# (Starting as a daemon means the thread will not prevent the process from exiting.)
-#tr_tcp.daemon = True
-tr_tcp.start()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @asyncio.coroutine
-def websocket_handler(websocket, path):
+def generate_content_cr():
+    print ("Starting generate content:")
+    sys.stdout.flush()
 
-    print("Connection openned")
-    #websocket.enableTrace(True)
+    while(1):
+        time.sleep(1)
 
+        with websockets_lock:
+            print ("Sending content..")
+            print ("NWebsockets: %d" % len(websockets))
+            for websocket in websockets:
+                pass
 
-    init_data = [
-            {
-            'msg-type':'cfg-process-mgr-list',
-            'process_mgrs': list( connected_processes.values() )
-            }
-            ]
-
-    pp.pprint(connected_processes)
-
-
-    init_data_s = json.dumps(init_data, separators=(',',':'))
-    yield from websocket.send(init_data_s)
-
-    # Add the websocket to the list:
-    with websockets_lock:
-        websockets[websocket] = None
+                if websocket.open:
+                    yield from websocket.send(send_data)
 
 
-    while 1:
-
-
-        msg_ins  = yield from websocket.recv()
-        #print ("Data in!! %s", msg_ins)
-
-        # Has the socket closed?
-        if msg_ins is None:
-            print("Socket closed. (By remote?)")
-            with websockets_lock:
-                del websockets[websocket]
-            return
-
-
-        # Otherwise, lets handle the data:
-        msgs = json.loads(msg_ins)
-
-        for msg in msgs:
-            msg_type = msg['msg-type']
-
-            if msg_type=="set-process-mgr":
-
-                mgr_id = int( msg['process-mgr-id'] )
-                process_mgr_data = connected_processes[mgr_id]
-
-                return_msg = {'msg-type': 'cfg-process-mgr-details'}
-                return_msg.update(process_mgr_data)
-
-
-                init_data_s = json.dumps([return_msg], separators=(',',':'))
-                yield from websocket.send(init_data_s)
-
-                # And store which process_mgr the websocket is interested in:
-                with websockets_lock:
-                    websockets[websocket] = mgr_id
-
-            else:
-                print ("Unhandled msg-type: %s" % msg_type)
-
-
-
-
-
-
-
-
-
-def websockets_thread():
+def generate_content_thread():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    start_server = ws.serve(websocket_handler, 'localhost', common.Ports.Websocket)
-    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_until_complete(generate_content_cr())
     asyncio.get_event_loop().run_forever()
 
 
 
-# And launch the websockets server in a new thread::
-print ("Launching websockets server:")
-tr_websockets = Thread(target=websockets_thread)
+
+print("Starting autogenerate thread")
+tr_autogen = Thread(target=generate_content_cr)
 # (Starting as a daemon means the thread will not prevent the process from exiting.)
-#tr_websockets.daemon = True
-tr_websockets.start()
+#tr_autogen.daemon = True
+tr_autogen.start()
+
+print("Looping")
+while(1):
+    time.sleep(1)
+
+
+print("jlkjl")
 
 
 
@@ -408,14 +131,291 @@ tr_websockets.start()
 
 
 
-
-
-
-try:
-    #tr.join()
-    tr_websockets.join()
-    tr_tcp.join()
-except:
-    print( "Exception Raise")
-    print( "TODO:: KILL THREADS")
-    raise
+##  class ThreadedTCPRequestHandler(socketserver.StreamRequestHandler):
+##  
+##      def handle(self):
+##  
+##          self.procmgr_id = None
+##  
+##          while True:
+##              # Msgs are "SIZE:PORT:CONTENTS"
+##              print ("Waiting for msg...")
+##  
+##              msg_size = self.rfile.readline()
+##              if not msg_size:
+##                  return
+##              msg_size = int(msg_size)
+##              msg_subport = self.rfile.readline()
+##              if not msg_subport:
+##                  return
+##  
+##              buff = b''
+##              while len(buff) < msg_size:
+##                  to_read = msg_size - len(buff)
+##                  x = self.rfile.read(to_read)
+##                  if not x:
+##                      print( 'Connection closed')
+##                      return
+##                  buff += x
+##  
+##              subport = int(msg_subport.decode('utf-8').strip() )
+##              msg = buff.decode('utf-8')
+##  
+##  
+##              print("Sending empty message to all websockets:")
+##              for websocket in websockets:
+##                  print("  -- %s" % websocket)
+##                  websocket.send("[]")
+##  
+##  
+##              #print ('Msg read OK: %d bytes over subport: %d' % ( len(msg), subport ) )
+##              #print(self.handleMsgMJHX)
+##              self.handleMsgMJHX( subport=subport, msg=msg )
+##              #print ("Handled OK!\n")
+##  
+##  
+##  
+##      def handleMsgMJHX(self, subport, msg):
+##          print ('Handling Message Port:%d Length:%d' %(subport, len(msg)) )
+##          print (msg)
+##          print ('\n')
+##  
+##          if subport == 0:
+##              print("Hooking in!")
+##              msg = json.loads(msg)
+##              print (msg)
+##  
+##              with websockets_lock:
+##  
+##                  print("New Process Mgr Connected")
+##  
+##                  if connected_processes:
+##                      self.procmgr_id =  max( [ mgr['id'] for mgr in connected_processes.values() ]) + 1
+##                      next_proc_id = max( [ proc['id']  for mgr in connected_processes.values() for proc in mgr['processes'] ]) + 1
+##                  else:
+##                      self.procmgr_id = 0
+##                      next_proc_id = 0
+##  
+##                  msg['id'] = self.procmgr_id
+##                  for (i,process) in enumerate(msg['processes']):
+##                      process['id'] = next_proc_id + i
+##  
+##                  # Add to the list:
+##                  connected_processes[self.procmgr_id] = msg
+##                  pp.pprint(connected_processes)
+##                  print("Connected to %d" % len(connected_processes) )
+##  
+##  
+##          else:
+##              print("Forwarding message....")
+##              subport_str = str(subport)
+##              # Lookup the port:
+##              with websockets_lock:
+##                  process, pipe_name = None,None
+##                  for proc in connected_processes[self.procmgr_id]['processes']:
+##                      #print ("AA")
+##                      #print (proc['outpipes'])
+##                      if subport_str in proc['outpipes']:
+##                          pipe_name = proc['outpipes'][subport_str]
+##                          process=proc
+##                          break
+##                      #print ("BB")
+##                  #print("Subport_str: %s" %subport_str)
+##                  #print("Pipename: %s" %pipe_name)
+##  
+##                  assert(pipe_name)
+##                  assert(proc)
+##                  #print ("Found pipename: %s", pipe_name)
+##  
+##  
+##                  # Ok, we can now send out a packet to each websocket:
+##                  std_pkt = [ {'msg-type':'output','process_id':process['id'], 'pipe':pipe_name, 'contents':msg}]
+##                  std_pkt_json = json.dumps(std_pkt)
+##  
+##  
+##                  #with websockets_lock:
+##                  #print ("Sending content..")
+##                  #print ("NWebsockets: %d" % len(websockets))
+##                  for (websocket, ws_procmgr_id) in websockets.items():
+##                      #print([self.procmgr_id, ws_procmgr_id])
+##                      if self.procmgr_id != ws_procmgr_id:
+##                          continue
+##  
+##                      if websocket.open:
+##                          print("Forwarding to websocket....%d", (websocket))
+##                          #assert(0)
+##                          websocket.send(std_pkt_json)
+##                          print("Sent...")
+##  
+##      def finish(self):
+##          print ("Closing up socket")
+##          if not self.procmgr_id:
+##              return
+##          with websockets_lock:
+##              del connected_processes[self.procmgr_id]
+##  
+##  
+##  
+##  
+##  class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+##      pass
+##  
+##  
+##  
+##  def build_tcpserver():
+##      # Port 0 means to select an arbitrary unused port
+##      HOST, PORT = "localhost", common.Ports.ProcessMgrs
+##  
+##      server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+##      ip, port = server.server_address
+##  
+##      # Start a thread with the server -- that thread will then start one
+##      # more thread for each request
+##      server_thread = threading.Thread(target=server.serve_forever)
+##      # Exit the server thread when the main thread terminates
+##      server_thread.daemon = True
+##      server_thread.start()
+##  
+##  
+##  
+##  print ("Launching tcp server:")
+##  tr_tcp = Thread(target=build_tcpserver)
+##  # (Starting as a daemon means the thread will not prevent the process from exiting.)
+##  #tr_tcp.daemon = True
+##  tr_tcp.start()
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  @asyncio.coroutine
+##  def websocket_handler(websocket, path):
+##  
+##      print("Connection openned")
+##      #websocket.enableTrace(True)
+##  
+##  
+##      init_data = [
+##              {
+##              'msg-type':'cfg-process-mgr-list',
+##              'process_mgrs': list( connected_processes.values() )
+##              }
+##              ]
+##  
+##      pp.pprint(connected_processes)
+##  
+##  
+##      init_data_s = json.dumps(init_data, separators=(',',':'))
+##      yield from websocket.send(init_data_s)
+##  
+##      # Add the websocket to the list:
+##      with websockets_lock:
+##          websockets[websocket] = None
+##  
+##  
+##      while 1:
+##  
+##  
+##          msg_ins  = yield from websocket.recv()
+##          #print ("Data in!! %s", msg_ins)
+##  
+##          # Has the socket closed?
+##          if msg_ins is None:
+##              print("Socket closed. (By remote?)")
+##              with websockets_lock:
+##                  del websockets[websocket]
+##              return
+##  
+##  
+##          # Otherwise, lets handle the data:
+##          msgs = json.loads(msg_ins)
+##  
+##          for msg in msgs:
+##              msg_type = msg['msg-type']
+##  
+##              if msg_type=="set-process-mgr":
+##  
+##                  mgr_id = int( msg['process-mgr-id'] )
+##                  process_mgr_data = connected_processes[mgr_id]
+##  
+##                  return_msg = {'msg-type': 'cfg-process-mgr-details'}
+##                  return_msg.update(process_mgr_data)
+##  
+##  
+##                  init_data_s = json.dumps([return_msg], separators=(',',':'))
+##                  yield from websocket.send(init_data_s)
+##  
+##                  # And store which process_mgr the websocket is interested in:
+##                  with websockets_lock:
+##                      websockets[websocket] = mgr_id
+##  
+##              else:
+##                  print ("Unhandled msg-type: %s" % msg_type)
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  def websockets_thread():
+##      loop = asyncio.new_event_loop()
+##      asyncio.set_event_loop(loop)
+##      start_server = ws.serve(websocket_handler, 'localhost', common.Ports.Websocket)
+##      asyncio.get_event_loop().run_until_complete(start_server)
+##      asyncio.get_event_loop().run_forever()
+##  
+##  
+##  
+##  # And launch the websockets server in a new thread::
+##  print ("Launching websockets server:")
+##  tr_websockets = Thread(target=websockets_thread)
+##  # (Starting as a daemon means the thread will not prevent the process from exiting.)
+##  #tr_websockets.daemon = True
+##  tr_websockets.start()
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  
+##  try:
+##      #tr.join()
+##      tr_websockets.join()
+##      tr_tcp.join()
+##  except:
+##      print( "Exception Raise")
+##      print( "TODO:: KILL THREADS")
+##      raise
---
+++ src/processmanager/central_processmgr_tracker_workingish.py
@@ -0,0 +1,115 @@
+#! /usr/bin/python3
+
+
+
+#processes = [
+#    # id, caption, command, 
+#    (None, "Proc1", """cat "hello" """,)
+#    (None, "Proc2", """cat "hello" """,)
+#
+#        ]
+import logging
+logger = logging.getLogger('websockets.server')
+logger.setLevel(logging.ERROR)
+logger.addHandler(logging.StreamHandler())
+
+
+import asyncio
+import websockets as ws
+
+import multiprocessing
+#from multiprocessing import Process, Queue
+from threading import Thread, Lock
+import time
+import zmq
+websockets = []
+websockets_lock = Lock()
+
+
+
+#@asyncio.coroutine
+#def hello(websocket, path):
+#
+#    # Add the websocket to the list:
+#    with websockets_lock:
+#        websockets.append(websocket)
+#
+#    print ("Connection openned")
+#    name = yield from websocket.recv()
+#    print("< {}".format(name))
+#    greeting = "Hello {}!".format(name)
+#    yield from websocket.send(greeting)
+#    print("> {}".format(greeting))
+#
+#    websocket.send("JLKJ")
+#
+#    name = yield from websocket.recv()
+#    print("< {}".format(name))
+#    greeting = "Hello - round2 {}!".format(name)
+#    yield from websocket.send(greeting)
+#    print("> {} (round2)".format(greeting))
+#
+#    # Remove the websocket:
+#    with websockets_lock:
+#        websockets.remove(websocket)
+
+
+
+
+
+
+@asyncio.coroutine
+def generate_content_cr(): 
+    print ("Starting generate content:")
+
+    port = "5556"
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    socket.bind("tcp://*:%s" % port)
+
+    while(1):
+        time.sleep(1)
+        #send_data = "GENERATED_CONTENT"
+        send_data = socket.recv()
+        send_data = str(send_data)
+        print (send_data, type(send_data))
+        #send_data = "jlkjl"
+
+        with websockets_lock:
+            print ("Sending content..")
+            print ("NWebsockets: %d" % len(websockets))
+            for websocket in websockets:
+
+                if websocket.open:
+                    yield from websocket.send(send_data)
+
+
+def generate_content_thread(): 
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    asyncio.get_event_loop().run_until_complete(generate_content_cr())
+    asyncio.get_event_loop().run_forever()
+
+
+
+
+# Spawn off the thread to take of reading input and forwarding it up to the web-sockets:
+print ("Started thread")
+tr = Thread(target=generate_content_thread, )
+tr.start()
+
+
+while(1):
+    print("Sleeping")
+    time.sleep(1)
+
+
+## And launch the websockets server:
+#print ("Launching server:")
+#start_server = ws.serve(hello, 'localhost', 8765)
+#asyncio.get_event_loop().run_until_complete(start_server)
+#asyncio.get_event_loop().run_forever()
+
+
+
+
